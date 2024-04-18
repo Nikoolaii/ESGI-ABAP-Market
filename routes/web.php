beaf282b146c9f07eed368502d357f11f3e47e79
Nikoolaii <nikolailemerre@gmail.com>
2024-04-18 12:36:45 +0200

    Edit login and register redirect

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\AdminController;
 
-
 Route::get('/', function () {
     return view('home');
 });
