5a97698fb94859ab0c4f3cd89f71c8169a3aeda4
Roelstrate Thibault <troelstrate2@myges.fr>
2024-04-18 12:39:16 +0200

    Up

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
+
 
 
 Route::get('/', function () {
